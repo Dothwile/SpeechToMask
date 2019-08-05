bd213a27492bd49d64f1a686c3bffdd35db41db4
Artur Smiechowski <art27833@gmail.com>
2019-08-05 15:09:54 -0400

    Text slicing/formatting for text_packer done, serial comms in the works

--- TextHandler.py
+++ TextHandler.py
@@ -5,33 +5,50 @@ from collections import deque
 
 class TextHandler:
 
-    # Packages text from SpeechDetector and communicates it to arduino
+    # Packages text from SpeechDetector and communicates it to Teensy LC, (arduino if you nasty)
 
     def __init__(self):
-        self.comm = serial.Serial("/dev/ttyACM0", 9600)
+        self.comm = serial.Serial("/dev/ttyACM0", 115200)
 
 
     def setup_comm(self):
         # Set up serial comm with MCU
-    handshake = False
-    while not handshake:
-        # Serial write a connection message
-        time.sleep(100)
-        # Serial Read for response
-        if True:
-            handshake = True
+        self.comm.open()
+        handshake = False
+        while not handshake:
+            # Serial write a connection message
+            self.comm.write()
+            time.sleep(.5)
+            # Serial Read for response
+            if True:
+                handshake = True
 
     def text_packer(self):
         # Packs text for display on mask
-        cur_chars_l1 = 0
-        cur_chars_l2 = 0
         to_write_l1 = ""
         to_write_l2 = ""
-        
+        while len(to_write_l1) <= 7:
+            cur_word = Main.word_que.pop(0)
+            if len(cur_word) > 7 and len(to_write_l1) < 4:
+                to_write_l1 = to_write_l1 + cur_word[0:2].capitalize() + ". "
+            elif len(cur_word) < (7 - len(to_write_l1)):
+                to_write_l1 = to_write_l1 + cur_word.capitalize() + " "
+            else
+                to_write_l2 = cur_word.capitalize()
+        while len(to_write_l2) <= 7:
+            cur_word = Main.word_que.pop(0)
+            if len(cur_word) > 7 and len(to_write_l2) < 4:
+                to_write_l2 = to_write_l2 + cur_word[0:2] + ". "
+            elif len(cur_word) < (7 - len(to_write_l2)):
+                to_write_l2 = to_write_l2 + cur_word.capitalize() + " "
+            else
+                Main.word_que.appendleft(cur_word)
+                break
+
+        # TODO Write out via serial
 
 
 
-        
 
 
     def run(self):
@@ -39,3 +56,4 @@ class TextHandler:
         while True:
             if len(Main.word_que) != 0:
                 self.text_packer()
+                time.sleep(.25)
